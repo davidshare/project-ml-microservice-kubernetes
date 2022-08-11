537816097c2630da527e1f32afd00251d3443d61
David Essien <davidessienshare@gmail.com>
2022-08-11 01:16:39 +0100

    update logging and kubernetes bash script

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -6,11 +6,11 @@
 dockerpath=davidshare/housing-prices
 
 # Run the Docker Hub container with kubernetes
-kubectl create deployment nginx --image=davidshare/housing-prices --replicas=3
+kubectl run housing-prices --image=$dockerpath
 
 # list the pods
 kubectl get pods
 
 # Forward the container port to a host
-kubectl port-forward capstone-devops 8080:80
+kubectl port-forward housing-prices 8080:80
 
